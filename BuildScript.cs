edb09a12dd84ec6cf29278bfe4b7ebaf2f14edd4
rasco1999 <r7ipmu@gmail.com>
2025-05-23 05:42:12 +0300

    Add files via upload

--- BuildScript.cs
+++ BuildScript.cs
@@ -1,20 +1,42 @@
 
 using UnityEditor;
 using System.IO;
+using UnityEditor.Build.Reporting;
 
 public class BuildScript
 {
     public static void BuildAndroid()
     {
-        string path = "Builds/Android";
-        if (!Directory.Exists(path))
-            Directory.CreateDirectory(path);
+        string buildPath = "Builds/Android";
+        string apkName = "PizzaGame.apk";
 
-        BuildPipeline.BuildPlayer(
-            new[] { "Assets/Scenes/Main.unity" },
-            path + "/PizzaGame.apk",
-            BuildTarget.Android,
-            BuildOptions.None
-        );
+        if (!Directory.Exists(buildPath))
+        {
+            Directory.CreateDirectory(buildPath);
+        }
+
+        string[] scenes = new string[]
+        {
+            "Assets/Scenes/Main.unity"
+        };
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            locationPathName = Path.Combine(buildPath, apkName),
+            target = BuildTarget.Android,
+            options = BuildOptions.None
+        };
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+        if (report.summary.result == BuildResult.Succeeded)
+        {
+            UnityEngine.Debug.Log("Build succeeded: " + report.summary.totalSize + " bytes");
+        }
+        else
+        {
+            UnityEngine.Debug.LogError("Build failed");
+        }
     }
 }
